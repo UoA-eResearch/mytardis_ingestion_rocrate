19273807772c4d51fd343ac528a1c5cf0011706c
JloveUOA <james.love@auckland.ac.nz>
2024-09-24 15:29:24 +1200

    remove default MT user pubkey

--- src/ingestion_targets/print_lab_genomics/__init__.py
+++ src/ingestion_targets/print_lab_genomics/__init__.py
@@ -8,11 +8,13 @@ from mytardis_rocrate_builder.rocrate_dataclasses.rocrate_dataclasses import (
 
 from src.mt_api.mt_consts import UOA
 
+
+#default MyTardis user, overwritten by values in env
 MY_TARDIS_TEST_USER = User(
     identifier="MyTardis",
     name="MyTardisUser",
-    email="james.love@auckland.ac.nz",
-    pubkey_fingerprints=["C0626AEA19335E5587944C94EE7395E1CBF7668B"],
+    email="test.mytardis@notavalidemail.com",
+    pubkey_fingerprints=[],
     affiliation=UOA,
     mt_identifiers=[],
 )
